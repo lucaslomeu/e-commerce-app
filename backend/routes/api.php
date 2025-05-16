aedc3effcd2e46def5f70ee35f07a4a371483545
Lucas Lomeu <lucaslomeugomes@gmail.com>
2025-05-16 09:42:00 -0300

    [FEAT] Initialize nuxt

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -5,3 +5,9 @@ use App\Http\Controllers\Api\ProductController;
 Route::apiResource('products', ProductController::class);
 
 
+// Se eu quiser definir cada rota manualmente, posso fazer assim:
+// Route::get('/products', [ProductController::class, 'meuIndex']);
+// Route::get('/products/{id}', [ProductController::class, 'visualizar']);
+// Route::post('/products', [ProductController::class, 'criar']);
+// Route::put('/products/{id}', [ProductController::class, 'atualizar']);
+// Route::delete('/products/{id}', [ProductController::class, 'remover']);
